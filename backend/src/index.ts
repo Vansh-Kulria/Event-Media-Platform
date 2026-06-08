1a80f1cf311e84242c6794eec1ea6bc9494f64f5
Vansh Kulria <vanshkulria05@gmail.com>
2026-06-08 16:22:43 +0530

    first user creataed

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
