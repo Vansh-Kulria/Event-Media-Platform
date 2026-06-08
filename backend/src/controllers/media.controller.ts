7a3d71a93c1a4faed272254a5137bc75cbd67880
Vansh Kulria <vanshkulria05@gmail.com>
2026-06-09 00:17:18 +0530

    media uploading and deleting in db

--- backend/src/controllers/media.controller.ts
+++ backend/src/controllers/media.controller.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import prisma from "../lib/prisma";
 import { AuthRequest } from "../middleware/auth.middleware";
 
@@ -30,6 +30,81 @@ export const uploadMedia = async (
 
     res.status(500).json({
       message: "Upload failed",
+    });
+  }
+};
+
+interface EventParams {
+  eventId: string;
+}
+
+export const getEventMedia = async (
+  req: Request<EventParams>,
+  res: Response
+) => {
+  try {
+    const eventId = req.params.eventId;
+
+    const media = await prisma.media.findMany({
+      where: {
+        eventId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    res.json(media);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to fetch media",
+    });
+  }
+};
+
+export const deleteMedia = async (
+  req: AuthRequest,
+  res: Response
+) => {
+  try {
+    const mediaId = req.params.id as string;
+
+    const media = await prisma.media.findUnique({
+      where: {
+        id: mediaId,
+      },
+    });
+
+    if (!media) {
+      return res.status(404).json({
+        message: "Media not found",
+      });
+    }
+
+    if (
+      media.uploadedById !== req.user!.userId
+    ) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    await prisma.media.delete({
+      where: {
+        id: mediaId,
+      },
+    });
+
+    res.json({
+      message: "Media deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to delete media",
     });
   }
 };
--- backend/src/routes/media.routes.ts
+++ backend/src/routes/media.routes.ts
@@ -3,19 +3,29 @@ import { Router } from "express";
 import upload from "../middleware/upload.middleware";
 
 import { authenticate }
-from "../middleware/auth.middleware";
+    from "../middleware/auth.middleware";
 
 import {
   uploadMedia,
+  getEventMedia,
+  deleteMedia,
 } from "../controllers/media.controller";
 
 const router = Router();
 
 router.post(
-  "/upload",
+    "/upload",
+    authenticate,
+    upload.single("file"),
+    uploadMedia
+);
+    
+router.get("/events/:eventId", getEventMedia);
+
+router.delete(
+  "/:id",
   authenticate,
-  upload.single("file"),
-  uploadMedia
+  deleteMedia
 );
 
 export default router;
